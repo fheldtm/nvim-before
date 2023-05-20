27dbf17b581547681bfbaad6376d92b118041223
fheldtm <fheldtm@gmail.com>
2023-05-20 10:25:28 +0900

    Ctrl V to Nop

--- lua/cjlee/remap.lua
+++ lua/cjlee/remap.lua
@@ -51,4 +51,5 @@ vim.keymap.set('n', 's-', '<C-w>-')
 -- default key unbind
 vim.keymap.set('n', 's', '<Nop>', { silent = true })
 vim.keymap.set('n', 'S', '<Nop>', { silent = true })
+vim.keymap.set('n', '<C-v>', '<Nop>', { silent = true })
 
